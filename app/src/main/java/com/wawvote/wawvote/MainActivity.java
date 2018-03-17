06d546dc5e8f675a3c74e94d7ab5bd683e12e6fa
Paweł Wangryn <pwangryn@op.pl>
2018-03-17 16:45:50 +0100

    Initial commit

---
+++ app/src/main/java/com/wawvote/wawvote/MainActivity.java
@@ -0,0 +1,28 @@
+package com.wawvote.wawvote;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v) {
+        //Button button =(Button) v;
+        startActivity(new Intent(getApplicationContext(), reczne.class));
+
+    }
+
+}
---
+++ app/src/main/java/com/wawvote/wawvote/reczne.java
@@ -0,0 +1,22 @@
+package com.wawvote.wawvote;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class reczne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reczne);
+    }
+
+    public void buttonOnClick(View v) {
+        //Button button =(Button) v;
+
+
+    }
+
+}
