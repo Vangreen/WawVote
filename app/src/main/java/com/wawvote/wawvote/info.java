779876d767c9008bf1d269ff60c3bcf4530f156c
Paweł Wangryn <pwangryn@op.pl>
2018-03-18 12:45:41 +0100

    Implement blockchain

--- app/src/main/java/com/wawvote/wawvote/info.java
+++ app/src/main/java/com/wawvote/wawvote/info.java
@@ -1,15 +1,28 @@
 package com.wawvote.wawvote;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.net.URI;
 
 public class info extends AppCompatActivity {
     private static ImageView foto;
+    private final String SERVER_URL = "http://192.168.137.127:8080";
+    private int getPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +34,7 @@ public class info extends AppCompatActivity {
         if(getIntent() != null)
         {
             String candidateName = getIntent().getStringExtra("candidateName");
-            int getPath = Integer.parseInt(getIntent().getStringExtra("imagePath"));
+            getPath = Integer.parseInt(getIntent().getStringExtra("imagePath"));
             try{
                 txtInfo.setText(candidateName);
                 foto.setImageResource(R.drawable.kandydat1+getPath);
@@ -39,5 +52,29 @@ public class info extends AppCompatActivity {
         }
 
 
+    }
+
+    protected void makeVote(int vote){
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, SERVER_URL+"/candidates/?id="+vote,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            requestQueue.stop();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    requestQueue.stop();
+                }
+            });
+            stringRequest.setRetryPolicy(new DefaultRetryPolicy(100000, 2, (float) 2.0));
+            requestQueue.add(stringRequest);
+    }
+
+    public void voteOnClick(View view) {
+        makeVote(getPath);
+        startActivity(new Intent(getApplicationContext(), end_screen.class));
     }
 }
