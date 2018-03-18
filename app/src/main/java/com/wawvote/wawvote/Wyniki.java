883da6c4cb9be3ba5cc3166e6227dc085f2c9738
Paweł Wangryn <pwangryn@op.pl>
2018-03-18 13:32:21 +0100

    Dodano ekran z wynikami

---
+++ app/src/main/java/com/wawvote/wawvote/Wyniki.java
@@ -0,0 +1,13 @@
+package com.wawvote.wawvote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Wyniki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wyniki);
+    }
+}
--- app/src/main/java/com/wawvote/wawvote/end_screen.java
+++ app/src/main/java/com/wawvote/wawvote/end_screen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class end_screen extends AppCompatActivity {
 
@@ -12,5 +13,10 @@ public class end_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+    }
+    public void wynikiOnClick(View v) {
+
+            startActivity(new Intent(getApplicationContext(), Wyniki.class));
+
     }
 }
